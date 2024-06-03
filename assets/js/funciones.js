54d4c17e7464d8a8294131cb7fb88be1d4803fff
Cristian Bernal <cristian.bernal.ortega@gmail.com>
2024-06-03 16:41:10 -0400

    Archivos comentados.

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,20 +1,24 @@
+// Programando el botón "Ver Más"
 function verMas(nombreCategoria) {
   const BORRARHTML = "";
   var tabla =
     '<table class="table rounded"><thead><tr class="table-info"><th scope="col">#</th><th scope="col">Preparación</th><th scope="col">Imagen</th></tr></thead><tbody id="body-preparaciones"></tbody></table>';
   $("#tabla-extra").html(BORRARHTML);
+  // Reduciendo la primera tabla
   $("#reducir-tabla").removeClass("col-lg-12").addClass("col-lg-6");
   $("#tabla-extra").addClass("col-lg-6").html(tabla);
   var htmlSpinner = '<div class="spinner mx-auto"></div>';
   $("#spinner").append(htmlSpinner);
+  // Consultando segundo link de la API según categoría
   $.get(
     "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + nombreCategoria,
     function (data) {
       $("#spinner").html("");
       var divNuevaTabla = '<div id="tabla-extra" class="col-lg-6"></div>';
-
+      // Creando espacio para la segunda tabla
       $("#tabla-row-2").append(divNuevaTabla);
       $("#body-preparaciones").html(BORRARHTML);
+      // Recorriendo datos consultados según categoría
       $.each(data.meals, function (i, items2) {
         var filaHtml =
           '<tr><th scope="row">' +
@@ -26,8 +30,6 @@ function verMas(nombreCategoria) {
           '" width="100"></td></tr>';
         $("#body-preparaciones").append(filaHtml);
       });
-      divNuevaTabla += "";
-      //$("#tabla-row").html(BORRARHTML);
     }
   );
 }
--- assets/js/prueba.js
+++ assets/js/prueba.js
@@ -1,22 +1,32 @@
-//Inicianco Jquery
+// Iniciando jQuery
 $(document).ready(function () {
-
+  // Creando constante para reiniciar el HTML
   const BORRARHTML = "";
+  // Asignando estructura de la tabla a poblar en un formato String
   var divTabla =
-    '<div id="reducir-tabla" class="col-lg-12"><table class="table border border-primary"><thead><tr class="table-info border border-primary rounded"><th scope="col">#</th><th scope="col">Nombre</th><th scope="col">Descripción</th><th scope="col">Acciones</th></tr></thead><tbody id="body-categorias"></tbody></table></div><div id="tabla-extra" class="rounded"></div>';
+    '<div id="reducir-tabla" class="col-lg-12"><table class="table"><thead><tr class="table-info rounded"><th scope="col">#</th><th scope="col">Nombre</th><th scope="col">Descripción</th><th scope="col">Acciones</th></tr></thead><tbody id="body-categorias"></tbody></table></div><div id="tabla-extra"></div>';
 
+  // Agregando la estructura de la tabla al HTML
   $("#tabla-row").append(divTabla);
 
+  // Utilizando las funcionalidades de jQuery para ejecutar una función al hacer click en el
+  // elemento designado con el id consultar
   $("#consultar").on("click", function () {
     var htmlSpinner = '<div class="spinner mx-auto"></div>';
     $("#body-categorias").html(BORRARHTML);
+    // Agregando el spinner
     $("#spinner").append(htmlSpinner);
 
+    // Consultando la API
     $.get(
       "https://www.themealdb.com/api/json/v1/1/categories.php",
       function (data) {
+        // Quitando el spinner al cargar la API
         $("#spinner").html("");
+        // Recorriendo el apartado de "categories" del JSON
         $.each(data.categories, function (i, items) {
+          // Guardando la estructura de la tabla junto a la información en la API
+          // en un tipo de dato String
           var filaHtml =
             '<tr id="fila-' +
             items.idCategory +
@@ -29,6 +39,7 @@ $(document).ready(function () {
             "</td><td><button onclick=\"verMas('" +
             items.strCategory +
             '\')" class="btn btn-info"><i class="fa-solid fa-magnifying-glass-plus"></i> Ver más</button></td></tr>';
+          // Agregando cada fila de información al HTML
           $("#body-categorias").append(filaHtml);
         });
       }
